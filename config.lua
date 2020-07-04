36885b4bbafab9043f60cdae162528f0e14ea946
m3 <54166713+morpheause@users.noreply.github.com>
2020-07-04 22:57:03 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,9 +4,19 @@ Config.UseM3Dispatch = false -- dont touch this
 
 Config.NeededCops = 0
 
-Config.SafeRewardMin = 4000
+Config.SafeRewardMin = 3000
 
-Config.SafeRewardMax = 12000
+Config.SafeRewardMax = 8000
 
 Config.LockTolerance = 5
+
+Config.StartSearchTime = 30000
+
+-- discord webhook --
+Config.EnableDiscordLog = false
+Config.DiscordWebhook = 'discord webhook url'
+Config.WebhookName = 'm3 logger' --webhook name
+Config.WebhookAvatarUrl = 'https://i.imgyukle.com/2020/05/11/r8k2GI.png' -- webhook avatar url
+---------------------
+
 --morphease#3333--
